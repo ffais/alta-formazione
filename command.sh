67584f8a4c4f291e482ddf2ddde2baedea272521
ffais <ffais@fbk.eu>
2019-09-02 09:18:15 +0200

    first commit

---
+++ command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export DOCKER_BUILDKIT=1
+docker build -f Dockerfile-experimental -t registry .
+docker run --network altaformazione --name zipkin -p 9411:9411 -d openzipkin/zipkin
+docker run --network altaformazione --name mongo -d mongo:latest
+docker run --memory 256m --network altaformazione -p 8761:8761 --name registry -d registry
+docker run --network altaformazione --memory 256m --name catalog -p 18080:8080 -e "MONGO_URL=mongo" -e "ZIPKIN_URL=http://zipkin:9411" -e "EUREKA_URL=http://registry:8761/eureka" -d catalog
+docker run --network altaformazione --memory 256m --name purchase -p 28080:8080 -e "MONGO_URL=mongo" -e "ZIPKIN_URL=http://zipkin:9411" -e "EUREKA_URL=http://registry:8761/eureka" -d purchase
+docker run --network altaformazione --memory 256m --name zuul-proxy -p 5000:5000 -e "ZIPKIN_URL=http://zipkin:9411" -e "EUREKA_URL=http://registry:8761/eureka" -d zuul-proxy
+
+
+declare -i i;i=0; while true; do i+=1; echo $i; curl -X GET -H "Content-Type: application/json" 'http://catalogo.local/api/products'; echo ""; sleep 2; done;
